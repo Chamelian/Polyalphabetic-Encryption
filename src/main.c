5088a1ee1d3e49d4d1c9908c0319ed5edd1b2c01
Chamelian <meliachameli@gmail.com>
2026-02-21 18:49:06 -0700

    fixed main to return int

--- src/main.c
+++ src/main.c
@@ -3,9 +3,6 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define encode 0
-#define decode 1
-
 const int ALPHABET_COUNT = 5;
 const char alphabet[] = {
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
@@ -132,7 +129,7 @@ void _fileEncryption(char readFilePath[], char writeFilePath[], const char alpha
 
 
 
-void main() {
+int main(int argc, char *argv[]) {
 
     printf("Seed (Primary Key): %d\n", seed);
     srand(seed);
@@ -162,4 +159,6 @@ void main() {
     //     lemon,
     //     alphabetOrderKey
     // );
+
+    return 0;
 }
